828c87a9493541b06978ed3fe8eaa807b7facb7b
rubu <rubayethossain2003@gmail.com>
2026-08-26 21:21:29 +0600

    update build 2

--- storage/framework/views/84845e007e695e389430cea318dad093.php
+++ storage/framework/views/84845e007e695e389430cea318dad093.php
@@ -26,7 +26,7 @@
                     class="lg:hidden fixed top-0 left-0 bg-black size-full duration-300 z-999 opacity-0 visible pointer-events-none menu-close fade-overlay">
                 </div>
                 <div
-                    class="flex lg:justify-center lg:basis-auto lg:grow max-lg:flex-col justify-start text-light font-base max-lg:fixed max-lg:h-screen max-lg:px-5 max-lg:top-0 max-lg:-left-75 max-lg:z-9999 max-lg:bg-indigo-950 dark:max-lg:bg-dark max-lg:w-72 max-lg:overflow-auto max-lg:duration-700 header-nav custom-scroll">
+                    class="flex lg:justify-center lg:basis-auto lg:grow max-lg:flex-col justify-start text-white font-base max-lg:fixed max-lg:h-screen max-lg:px-5 max-lg:top-0 max-lg:-left-75 max-lg:z-9999 max-lg:bg-indigo-950 dark:max-lg:bg-dark max-lg:w-72 max-lg:overflow-auto max-lg:duration-700 header-nav custom-scroll">
                     <div class="flex items-center relative z-9 py-6.25 lg:hidden">
                         <!-- Logo -->
                         <a href="<?php echo e(route('home')); ?>" class="flex items-center space-x-3 rtl:space-x-reverse">
